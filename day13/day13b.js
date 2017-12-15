70ca7106e93db67b75e62a76451f5b44f98df73e
Eric Wagoner <eric.wagoner@iinteractive.com>
2017-12-15 18:06:35 -0500

    Day 13b

--- day13/day13b.js
+++ day13/day13b.js
@@ -1,7 +1,7 @@
 // http://adventofcode.com/2017/day/11
 // Part One
 
-var realinput = `0: 3
+var testinput = `0: 3
 1: 2
 4: 4
 6: 4`;
@@ -49,72 +49,28 @@ var input = `0: 3
 82: 17
 84: 14`;
 
-var turn = 0;
-var position = 0;
 var delay = 0;
 var caught = true;
 var inputArray = input.split("\n");
-for (i = 0; i < inputArray.length; i++) {
-    inputArray[i] = inputArray[i].split(": ");
-}
-var lastLayerIndex = parseInt(inputArray[inputArray.length - 1][0]);
 var layers = [];
-for (i = 0; i < lastLayerIndex + 1; i++) {
-    var depth =  null;
-    for (j = 0; j < inputArray.length; j++) {
-      if (inputArray[j][0] == i) {
-          depth = [parseInt(inputArray[j][1]), 0, "+"];
-      }
-    }
-    layers[i] = depth;
+for (i = 0; i < inputArray.length; i++) {
+    layers[i] = inputArray[i].split(": ");
 }
 
 while (caught == true) {
-    var safe = true;
-    //console.log(layers);
-    while (position <= lastLayerIndex) {
-        turn++;
-        position++;
-        //console.log("Turn: " + turn);
-        //console.log("position: " + position);
-        for (i = 0; i < lastLayerIndex + 1; i++) {
-            var layer = layers[i];
-            //console.log(layer);
-            if (layer != null) {
-                if ((position == i) && (layer[1] == 0)) {
-                    //console.log("Caught on turn " + turn + " at position " + position + " after a delay of " + delay);
-                    safe = false;
-                    turn = lastLayerIndex + 1;
-                }
-                if (layer[1] == 0) {
-                    layer[1] = 1;
-                    layer[2] = "+";
-                } else if (layer[1] == (layer[0] - 1)) {
-                    layer[1] = layer[1] - 1;
-                    layer[2] = "-";
-                } else if (layer[2] == "+") {
-                    layer[1] = layer[1] + 1;
-                } else {
-                    layer[1] = layer[1] - 1;
-                }
-                layers[i] = layer;  
-            }
+    var spotted = false;
+    for (i = 0; i < layers.length; i++) {
+        ticks = delay + parseInt(layers[i][0]) + 1;
+        if (((ticks - 1) % (2 * (parseInt(layers[i][1]) - 1))) == 0) {
+            spotted = true;
+            i = layers.length;         
         }
-    }   
-    if (safe == true) {
+    } 
+
+    if (spotted == false) {
         caught = false;
     } else {
         delay++;
-        for (i = 0; i < lastLayerIndex + 1; i++) {
-            if (layers[i] != null) {
-                layers[i][1] = 0;
-                layers[i][2] = "+";
-            }
-        }
-        turn = 0;
-        position = turn - 1 - delay;
-        console.log(delay);
-        //console.log(layers);
     }
 }
 
